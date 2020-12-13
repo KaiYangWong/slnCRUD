8c396d0599f5d52b8bb2bd7750f598ee8a9b01ee
Koby <koby412@hotmail.com.tw>
2020-12-13 10:33:39 +0800

    Complete Index() Function

---
+++ prjCRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using prjCRUD.Models; //引用要使用的Model
+
+namespace prjCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        CompanyEntities db = new CompanyEntities();
+
+        public ActionResult Index()
+        {
+            var employee = db.Employee.ToList();
+            return View(employee);
+        }
+
+    }
+}
